3d434997087758a7ef2a2f3eed52ed1e2dad7b92
Khushi Parekh <khushi4541@gmail.com>
2025-01-15 12:50:39 -0500

    conditionally rendered header and nav so they don't show up on landing page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
 import Logo from '../../assets/logo/levelup-logo.svg';
+import { useLocation } from 'react-router-dom';
 import "./Header.scss";
 
 function Header() {
+  const location = useLocation();
+  const isLandingPage = location.pathname === "/";
+
   return (
-    <section className="header">
+    <>
+    {!isLandingPage && ( <section className="header">
       <img src={Logo} alt='level up logo' />
       <div className="header__profile"></div>
-    </section>
+    </section>)}
+    </>
   );
 }
 
