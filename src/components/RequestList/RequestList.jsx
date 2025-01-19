9909d67e165181ccee6a9e91862d898573b034c9
Khushi Parekh <khushi4541@gmail.com>
2025-01-19 13:22:23 -0500

    created the header and search bar for the friends page

---
+++ src/components/RequestList/RequestList.jsx
@@ -0,0 +1,11 @@
+import "./RequestList.scss"
+
+function RequestList () {
+    return(
+        <section className="request-list">
+            <h3 className="request-list__title">Friend Requests</h3>
+        </section>
+    )
+}
+
+export default RequestList;
--- src/pages/FriendsPage/FriendsPage.jsx
+++ src/pages/FriendsPage/FriendsPage.jsx
@@ -1,7 +1,22 @@
-function FriendsPage () {
-    return (
-        <p>Friends Page</p>
-    )
+import VectorArt from "../../assets/images/friends.svg";
+import RequestList from "../../components/RequestList/RequestList";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import "./FriendsPage.scss";
+
+function FriendsPage() {
+  return (
+    <section className="friends-page">
+      <div className="friends-page__header">
+        <img
+          src={VectorArt}
+          alt="3 friends raising their arms in the air"
+          className="friends-page__image"
+        />
+      </div>
+      <SearchBar />
+      <RequestList />
+    </section>
+  );
 }
 
 export default FriendsPage;
