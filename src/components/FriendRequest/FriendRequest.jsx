82230186d04a5fef9172e3e93b40d32357c9b88e
Khushi Parekh <khushi4541@gmail.com>
2025-01-19 17:01:00 -0500

    created accept request functionality

--- src/components/FriendRequest/FriendRequest.jsx
+++ src/components/FriendRequest/FriendRequest.jsx
@@ -1,22 +1,64 @@
-import "./FriendRequest.scss"
-import profileImage from "../../assets/icons/account-icon.svg"
-import acceptIcon from "../../assets/icons/accept-icon.svg"
-import declineIcon from "../../assets/icons/decline-icon.svg"
+import "./FriendRequest.scss";
+import profileImage from "../../assets/icons/account-icon.svg";
+import acceptIcon from "../../assets/icons/accept-icon.svg";
+import declineIcon from "../../assets/icons/decline-icon.svg";
+import { baseURL } from "../../../utils/api";
+import axios from "axios";
+import { useState } from "react";
 
-function FriendRequest() {
+function FriendRequest({ request, handleAction }) {
+  const [message, setMessage] = useState("");
+  const requestId = request.id;
+  const authToken = localStorage.getItem("authToken")
+
+  const handleRequest = async (action) => {
+    try {
+      if (action === "accept") {
+        await axios.patch(
+          `${baseURL}/friends/request/${requestId}`,
+          null,
+          {
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+          }
+        );
+        setMessage("Friend request accepted!");
+      } else if (action === "decline") {
+        await axios.delete(`${baseURL}/friends/request/${requestId}`, {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        });
+        setMessage("Friend request declined.");
+      }
+      handleAction(requestId); 
+    } catch (error) {
+      console.error("Error processing friend request:", error);
+    }
+  };
+
   return (
-  <article className="request">
-    <img src={profileImage} alt="account profile placeholder" className="request__image" />
-    <p className="request__text"><span className="request__text--bold">Username</span> has sent you a friend request</p>
-    <div className="request__options">
-      <img src={acceptIcon} alt="" className="request__icon" />
-      <p className="request__action">Accept</p>
-    </div>
-    <div className="request__options">
-      <img src={declineIcon} alt="red X" className="request__icon" />
-      <p className="request__action">Decline</p>
-    </div>
-  </article>)
+    <article className="request">
+      <img
+        src={profileImage}
+        alt="account profile placeholder"
+        className="request__image"
+      />
+      <p className="request__text">
+        <span className="request__text--bold">{request.sender_username}</span>{" "}
+        has sent you a friend request
+      </p>
+      <div className="request__options">
+        <img src={acceptIcon} alt="purple checkmark" className="request__icon" onClick={() => handleRequest("accept")}/>
+        <p className="request__action">Accept</p>
+      </div>
+      <div className="request__options">
+        <img src={declineIcon} alt="red X" className="request__icon" />
+        <p className="request__action">Decline</p>
+      </div>
+    </article>
+  );
 }
 
 export default FriendRequest;
--- src/components/RequestList/RequestList.jsx
+++ src/components/RequestList/RequestList.jsx
@@ -1,13 +1,43 @@
+import { useState, useEffect } from "react";
 import FriendRequest from "../FriendRequest/FriendRequest";
-import "./RequestList.scss"
+import axios from "axios";
+import { baseURL } from "../../../utils/api";
+import "./RequestList.scss";
 
-function RequestList () {
-    return(
-        <section className="request-list">
-            <h3 className="request-list__title">Friend Requests</h3>
-            <FriendRequest/>
-        </section>
-    )
+function RequestList() {
+  const [friendRequests, setFriendRequests] = useState([]);
+  const authToken = localStorage.getItem("authToken");
+
+  useEffect(() => {
+    const fetchFriendRequests = async () => {
+      try {
+        const response = await axios.get(`${baseURL}/friends/requests`, {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        });
+        setFriendRequests(response.data);
+      } catch (error) {
+        console.error("Error fetching friend requests:", error);
+      }
+    };
+
+    fetchFriendRequests();
+  }, []);
+
+  const handleAction = (id) => {
+    setFriendRequests(friendRequests.filter((request) => request.id !== id));
+  };
+
+  return (
+    <section className="request-list">
+      <h3 className="request-list__title">Friend Requests</h3>
+      {friendRequests.length === 0 ? <p>Loading friend requests...</p> : ""}
+      {friendRequests.map((request) => (
+        <FriendRequest request={request} key={request.id} handleAction={handleAction}/>
+      ))}
+    </section>
+  );
 }
 
 export default RequestList;
